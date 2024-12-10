ea232d36a92e9b9c9c6f5565ad1a0a11c6c3f341
nuke cola <manaska129@gmail.com>
2024-12-10 19:10:00 +0700

    fuck i forgot
    pls masih belajar

---
+++ flutter_exp_first/lib/main_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wisata Bandung'),
+      ),
+    );
+  }
+}
